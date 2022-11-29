f44ba78a2567977d5db674a1225d60986d9e6817
SkiddedYouLOL <76093686+AlexTheAgent@users.noreply.github.com>
2022-11-28 21:52:38 -0500

    predictor.py
    Made with turtle python!

---
+++ Predictor.py
@@ -0,0 +1,36 @@
+
+
+ import turtle
+import frames
+
+
+  frame.create(new) 
+   frame.length(45, 50) 
+     frame.title("Predictor") 
+      frame.addtab("Mines", "Button", === tap.load(ent.frame) 
+      frame.addtab2("Towers", " Button", === tap2.load(ent.frame2) 
+  
+
+   if tab.frame(Show.All()) 
+       Grid = 
+  ( "❌", "❌", " ❌", "❌", " ❌", "❌", " ❌", "❌", " ❌", "❌", " ❌", "❌", " ❌", "❌")
+   Grid(A) = " ✅", 
+
+ Else
+    If tab.frame2(Show.all()))
+       TowersGrid =
+          wrongtowers1, "❌"
+          wrongtowers2, "❌"
+       Repeat(23) 
+           Correct.towers("✅") 
+
+Auth.token(ent.secret.auth) 
+    If secret.gone do
+     console.log("Token isn't there") 
+     Else
+        If secret.appeared do
+  console.log("Predictor loaded") 
+
+add.footer("Gui") 
+   Position === "23, 45, 84"
+      Text.footer === "credits to zoronsz") 
